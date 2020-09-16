eedfa573497705216ec2f9d856cfea047d37e948
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-16 11:12:24 -0700

    initial push

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+
+// import Header from './components/Header/Header.js';
+// import Form from './components/Form/Form.js';
+// import Output from './components/Output/Output.js';
+import Footer from '../src/components/Footer/Footer.js';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      method: '',
+      url: '',
+      count: 0,
+      results: [],
+      headers: {},
+    }
+  }
+
+  stateMethod = method => {
+    this.setState({ method });
+  }
+
+  stateURL = url => {
+    this.setState({ url });
+  }
+
+  listChar = (count, results, headers) => {
+    this.setState({ count, results, headers })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        {/* <Header />
+        <Form popStateMethod={this.stateMethod} popStateURL={this.stateURL} popList={this.listChar}/>
+        <Output method={this.state.method} url={this.state.url} count={this.state.count} results={this.state.results} headers={this.state.headers}/> */}
+        <Footer />
+
+      </div>
+    );
+  }
+}
+
+export default App;
