67760eb6901b9b1d880572e3dfa597801a0c2221
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-16 22:47:11 -0700

    started working on day 3 functionality, able to store request object

---
+++ src/components/History/History.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './History.scss';
+
+const History = (props) => {
+
+  const calls = props.calls || {};
+
+  function loadRequest(call) {
+    props.handleCall(call);
+  }
+
+
+  return (
+    <section className="History">
+      <h3 id="calls-header">Previous Calls:</h3>
+      
+      <ul id="history-list">
+        {
+          Object.keys(calls).map(key =>           
+            <li key={key}>
+              <span className={`method ${props.calls[key].method}`}>{props.calls[key].method}</span>
+              <button className="url" onClick={()=> loadRequest(props.calls[key])}>{props.calls[key].url}</button>
+            </li>,
+          )
+        }
+      </ul>
+ 
+    </section>
+  )
+
+}
+
+export default History;
